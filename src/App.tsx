b5892a2be237e3015ac3f813a9b01b2c3b95aeec
Hien Tran <hientran@Hiens-MacBook-Pro.local>
2025-06-01 11:25:19 +1000

    [quiz 8] updated code to add and then fetch cities

--- src/App.tsx
+++ src/App.tsx
@@ -46,15 +46,108 @@ function avgTemp(temps: number[]): number {
   return Math.round(temps.reduce((acc, curr) => acc + curr) / temps.length);
 }
 
+type TextInputProps = {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+};
+
+function TextInput({ label, value, onChange }: TextInputProps) {
+  return (
+    <div className="p-4">
+      <label htmlFor="input" className="block text-sm font-medium mb-2">
+        {label}
+      </label>
+      <input
+        id="input"
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="border border-gray-300 rounded p-2 w-full"
+        placeholder="Type here..."
+      />
+    </div>
+  );
+}
+
 function App() {
   const [cities, setCities] = useState<City[]>([]);
-
   const [selectedCategory, setSelectedCategory] = useState<
     TempCategory | "All"
   >("All");
-
+  const [minTempValue, setMinTempValue] = useState("");
+  const [newCity, setNewCity] = useState("");
+  const [newTemp, setNewTemp] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+
+  const onMinTempChange = (text: string) => {
+    console.log(text.trim());
+    setMinTempValue(text.trim());
+  };
+
+  const onNewCityChange = (text: string) => {
+    console.log(text.trim());
+    setNewCity(text.trim());
+  };
+
+  const onNewTempChange = (text: string) => {
+    console.log(text.trim());
+    setNewTemp(text.trim());
+  };
+
+  const fetchCities = () => {
+    fetch(
+      "http://localhost:8000/cities?min=" + (minTempValue! ? minTempValue : 0)
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Failed to fetch: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setCities(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+        setLoading(false);
+      });
+  };
+
+  const addNewCityAndTemp = (city: string, temp: string) => {
+    if (city == "" || temp == "") {
+      return;
+    }
+
+    const dataToSend = {
+      city: city,
+      temp: +temp,
+    };
+
+    fetch("http://localhost:8000/cities", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToSend),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Failed to fetch: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        fetchCities();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const grouped: Record<TempCategory, City[]> = cities.reduce((acc, curr) => {
     const cat = categorize(curr.temp);
@@ -128,23 +221,8 @@ function App() {
   };
 
   useEffect(() => {
-    fetch("http://localhost:8000/cities")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`Failed to fetch: ${res.status}`);
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setCities(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setError(err);
-        setLoading(false);
-      });
-  }, []);
+    fetchCities();
+  }, [minTempValue]);
 
   return (
     <div style={{ padding: "1rem" }}>
@@ -168,6 +246,37 @@ function App() {
           {cat}
         </button>
       ))}
+      <TextInput
+        label={"Minimum temperature:"}
+        value={minTempValue}
+        onChange={onMinTempChange}
+      />
+      <div>
+        <TextInput
+          label={"New city:"}
+          value={newCity}
+          onChange={onNewCityChange}
+        />
+        <TextInput
+          label={"New temp:"}
+          value={newTemp}
+          onChange={onNewTempChange}
+        />
+        <button
+          key={"addNewCityAndTemp"}
+          onClick={() => addNewCityAndTemp(newCity, newTemp)}
+          style={{
+            marginRight: "0.5rem",
+            padding: "0.5rem 1rem",
+            border: "none",
+            borderRadius: "4px",
+            cursor: "pointer",
+          }}
+        >
+          +Add City And Temp
+        </button>
+      </div>
+
       <h1>🌤️ City Temperature Dashboard</h1>
       {orderedCategories
         .filter((cat) => filtered[cat])
